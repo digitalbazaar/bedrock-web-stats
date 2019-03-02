e0a040d2a041b8980abfc13de1a88cde9c3cb842
Matthew Collier <collier.matthew@gmail.com>
2019-03-02 16:48:28 -0500

    Complete basic c3 implementation.

--- test/components/Home.vue
+++ test/components/Home.vue
@@ -10,6 +10,9 @@
         Get Stats
       </q-btn>
     </div>
+    <div style="min-height: 400px">
+      <div id="chart" />
+    </div>
     <div>
       <pre>
         {{reports}}
@@ -22,6 +25,9 @@
  * Copyright (c) 2019 Digital Bazaar, Inc. All rights reserved.
  */
 'use strict';
+
+// FIXME: c3 is loaded as a global in index.js as a hack
+/* globals c3 */
 
 import {StatsService} from 'bedrock-web-stats';
 
@@ -38,13 +44,62 @@ export default {
   },
   methods: {
     async getStats() {
+      const timestamps = ['x'];
+      const series1 = ['avgload'];
       const result = await this._statsService.get({
         // specify which monitors to pull
         monitorIds: ['os'],
         // get stats from the last 5 seconds
-        startDate: Date.now() - 5000
+        startDate: Date.now() - 50000
       });
-      this.reports = result;
+      for(const r of result) {
+        timestamps.push(r.createdDate);
+        series1.push(r.monitors.os.currentLoad.avgload);
+      }
+      const myChart = c3.generate({
+        data: {
+          x: 'x',
+          columns: [
+            timestamps,
+            series1,
+          ],
+          // type: 'spline'
+        },
+        zoom: {
+          enabled: true
+        },
+        axis: {
+          y: {
+            min: 0,
+            max: 1,
+            label: 'avgload',
+          },
+          x: {
+            type: 'timeseries',
+            tick: {
+              fit: true,
+              rotate: 45,
+              format: '%Y-%m-%d %H:%M:%S'
+            }
+          }
+        }
+      });
+      let lastTime = timestamps[timestamps.length - 1];
+      setInterval(async () => {
+        const timestamps = ['x'];
+        const series1 = ['avgload'];
+        const result = await this._statsService.get({
+          monitorIds: ['os'],
+          startDate: lastTime + 1
+        });
+        for(const r of result) {
+          timestamps.push(r.createdDate);
+          series1.push(r.monitors.os.currentLoad.avgload);
+        }
+        lastTime = timestamps[timestamps.length - 1];
+        myChart.flow({columns: [timestamps, series1]});
+      }, 1000);
+      // this.reports = result;
     },
     done() {
       console.log('Done!', this.form);
@@ -53,4 +108,5 @@ export default {
 };
 </script>
 <style>
+@import "https://cdnjs.cloudflare.com/ajax/libs/c3/0.6.12/c3.min.css"
 </style>
--- test/components/index.js
+++ test/components/index.js
@@ -20,16 +20,20 @@ brVue.setRootVue(async () => {
     Quasar,
     {default: iconSet},
     {default: Vuelidate},
-    c3
+    d3,
+    c3,
   ] = await Promise.all([
     import('bedrock-quasar'),
     import('quasar-framework'),
     import('quasar-framework/icons/fontawesome'),
     import('vuelidate'),
-    import ('https://cdnjs.cloudflare.com/ajax/libs/c3/0.6.12/c3.min.js')
+    // import ('https://cdnjs.cloudflare.com/ajax/libs/c3/0.6.12/c3.min.js')
+    import('https://cdnjs.cloudflare.com/ajax/libs/d3/5.9.1/d3.js'),
+    import('https://cdnjs.cloudflare.com/ajax/libs/c3/0.6.12/c3.js'),
   ]);
 
   // FIXME: this is a hack, do not use in production code
+  window.d3 = d3;
   window.c3 = c3;
 
   // replace default `br-root` with a custom one
